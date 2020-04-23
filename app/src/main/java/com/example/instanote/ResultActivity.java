149d9a53b767c27da188aa6dd953db00a444a57a
Kunal <kunal.bohra@somaiya.edu>
2020-04-24 00:01:17 +0530

    added MenuItemClickListener in ResultActivity

--- app/src/main/java/com/example/instanote/ResultActivity.java
+++ app/src/main/java/com/example/instanote/ResultActivity.java
@@ -6,13 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
     EditText resTitle;
     EditText resText;
+    boolean pinned = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,15 +27,29 @@ public class ResultActivity extends AppCompatActivity {
         resTitle = findViewById(R.id.resTitle);
         resText.setText(text);
         resTitle.setText(title);
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        boolean pinned = getIntent().getBooleanExtra(PinnedNotes.PINNED, false);
-        if (pinned) {
-            inflater.inflate(R.menu.menu, menu);
-        }
+        pinned = getIntent().getBooleanExtra(PinnedNotes.PINNED, false);
+        inflater.inflate(R.menu.menu, menu);
+        final MenuItem star = menu.findItem(R.id.save);
+        star.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (pinned == true) {
+                    star.setIcon(R.drawable.ic_star_white_24dp);
+                    pinned = false;
+                } else {
+                    star.setIcon(R.drawable.ic_star_border_white_24dp);
+                    pinned = true;
+                }
+                return true;
+            }
+        });
         return true;
     }
 }
