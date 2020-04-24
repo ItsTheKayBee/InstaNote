3482cc604bacdf56d6d937a8b53116965200c7c1
niddhhii <dedhianidhi21@gmail.com>
2020-04-24 12:38:38 +0530

    MainActivity listener added

--- app/src/main/java/com/example/instanote/MainActivity.java
+++ app/src/main/java/com/example/instanote/MainActivity.java
@@ -1,20 +1,48 @@
 package com.example.instanote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+    private SearchView searchView;
+    private ObjectAnimator objectAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchView = findViewById(R.id.search_bar);
+        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    objectAnimator = ObjectAnimator.ofFloat(searchView, "y", 50);
+                    objectAnimator.setDuration(300);
+                    objectAnimator.start();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainpage_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.pin){
+            Intent i=new Intent(MainActivity.this,PinnedNotes.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
