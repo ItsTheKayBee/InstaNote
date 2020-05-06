3df2a00226992c706b057d2079ca58958b0bad3f
niddhhii <dedhianidhi21@gmail.com>
2020-05-06 21:56:39 +0530

    Basic database connectivity added.

--- app/src/main/java/com/example/instanote/DatabaseOperations.java
+++ app/src/main/java/com/example/instanote/DatabaseOperations.java
@@ -1,7 +1,33 @@
 package com.example.instanote;
 
-public class DatabaseOperations {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseOperations extends SQLiteOpenHelper {
     //db code
     // crud operations
+    private static final int DATABASE_VERSION=1;
+    protected static final String DATABASE_NAME = "InstaNote";
 
+    public DatabaseOperations(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE PinnedNotes " +
+                "( id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "title TEXT, " +
+                "link TEXT, " +
+                "content TEXT ) ";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
